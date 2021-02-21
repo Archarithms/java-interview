a31e4548b8a1be195f2b56bc199656cae870ea35
Daniel Klatt <daniel.klatt@me.com>
2021-02-21 11:25:34 -0600

    Java Solution
    Added functions and tests to complete the Java solution of the coding challenge

--- java_solution/src/main/java/com/github/archarithms/App.java
+++ java_solution/src/main/java/com/github/archarithms/App.java
@@ -1,20 +1,47 @@
 package com.github.archarithms;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class App
 {
-  public static String convertToTitleCase(final String inpStr)
+  public static String convertToTitleCase(final String inpStr) throws Exception
   {
-    /**
-     * Convert all non-alphanumeric characters to spaces
-     * Remove leading and trailing whitespace
-     * Capitalize each word
-     */
-    return inpStr;
+    if (inpStr == null) {
+      throw new Exception();
+    }
+
+    String[] words = inpStr.split("[^a-zA-Z0-9]");
+    for (int i = 0; i < words.length; i++) {
+      String word = words[i];
+      String head = word.substring(0, 1);
+      String tail = word.substring(1);
+      words[i] = head.toUpperCase() + tail.toLowerCase();
+    }
+    return String.join(" ", words);
   }
 
-  public static String convertUnixToDateString(final Long inpUnixSeconds)
+  public static String convertUnixToDateString() throws Exception
   {
-    return String.valueOf(inpUnixSeconds);
+    Long seconds = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+
+    return convertUnixToDateString(seconds);
+  }
+
+  public static String convertUnixToDateString(final Long inpUnixSeconds) throws Exception
+  {
+    if (inpUnixSeconds == null) {
+      throw new Exception();
+    }
+
+    LocalDateTime date = LocalDateTime.ofEpochSecond(inpUnixSeconds, 0, ZoneOffset.UTC);
+    DateTimeFormatter formatter = patternedFormatter();
+
+    return date.format(formatter);
+  }
+
+  public static DateTimeFormatter patternedFormatter() {
+    return DateTimeFormatter.ofPattern("MMMM d, yyyy");
   }
 }
--- java_solution/src/test/java/com/github/archarithms/test/DateStringTests.java
+++ java_solution/src/test/java/com/github/archarithms/test/DateStringTests.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import com.github.archarithms.App;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * DateStringTests unit tests for simple App.
  */
@@ -20,17 +23,27 @@ public class DateStringTests
     /**
      * Test the testConvertUnixToDateString method
      */
-    // @Test
-    // public void testConvertUnixToDateString() {
-    //     String testStr = "July 4, 2017";
-    //     assertTrue(testStr.equals(App.convertUnixToDateString(1499144400L)));
-    // }
+    @Test
+    public void testConvertUnixToDateString() throws Exception {
+        String testStr = "July 4, 2017";
+        assertTrue(testStr.equals(App.convertUnixToDateString(1499144400L)));
+    }
 
     /**
      * Test the testNullCase method
      */
-    // @Test
-    // public void testNullCase() {
-    //     assertThrows(Exception.class, () -> App.convertUnixToDateString(null));
-    // }
+    @Test
+    public void testNullCase() throws Exception {
+        assertThrows(Exception.class, () -> App.convertUnixToDateString(null));
+    }
+
+    /**
+     * Test the testDefaultCase method
+     */
+    @Test
+    public void testDefaultCase() throws Exception {
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = App.patternedFormatter();
+        assertTrue(date.format(formatter).equals(App.convertUnixToDateString()));
+    }
 }
