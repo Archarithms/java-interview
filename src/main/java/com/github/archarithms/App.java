832613591a9b5876b2f9df7571ab426c103112e2
Michael Davis <michael.davis@posteo.net>
2022-07-19 20:33:54 -0500

    All tests passing. No failures, errors, or skipped tests.
    This commit satisfies the existing unit tests "as given".

--- src/main/java/com/github/archarithms/App.java
+++ src/main/java/com/github/archarithms/App.java
@@ -1,15 +1,49 @@
 package com.github.archarithms;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.WordUtils;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class App
 {
-  public static String convertToTitleCase(final String inpStr)
+  public static String convertToTitleCase(final String inpStr) throws NullPointerException
   {
-    return inpStr;
+    // Check for null input
+     if (inpStr == null){
+      throw new NullPointerException();
+    } 
+    
+    // Remove non-alphanumeric characters and replace them with spaces
+    String result = inpStr;
+    result = result.replaceAll("[^A-Za-z0-9]", " ");
+    
+    // Remove trailing spaces
+    result = StringUtils.stripEnd(result, " ");
+
+    // Set Correct Case
+    //result = StringUtils.lowerCase(result);
+    result = WordUtils.capitalizeFully(result);
+    
+    return result;
   }
 
-  public static String convertUnixToDateString(final Long inpUnixSeconds)
+  public static String convertUnixToDateString(final Long inpUnixSeconds) throws NullPointerException
   {
-    return String.valueOf(inpUnixSeconds);
+
+    LocalDate dt = LocalDate.now();
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM d, yyyy").withZone(ZoneId.systemDefault());
+    String result = null;
+    
+    // Return the current date if the input is null.
+     if (inpUnixSeconds == null){
+      result = dt.format(fmt).toString();
+      throw new NullPointerException();
+    } 
+
+    // Otherwise convert the unix epoch seconds to a datetime.
+    Instant inst = Instant.ofEpochSecond(inpUnixSeconds);
+    result = fmt.format(inst);
+    return result;
   }
 }
